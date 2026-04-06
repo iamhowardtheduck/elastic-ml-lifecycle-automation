e2933a072af493a6947dab65ca48eb54bb54093d
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-04-06 15:44:42 -0400

    Update bootstrap.sh

--- Scripts/bootstrap.sh
+++ Scripts/bootstrap.sh
@@ -1,5 +1,5 @@
 pip install elasticsearch faker
-python bootstrap-classification.py \
+python /workspace/workshop/elastic-ml-lifecycle-automation/bootstrap-classification.py \
     --host http://kubernetes-vm:30920 \
     --kibana-host http://kubernetes-vm:30002 \
     --user sdg \
