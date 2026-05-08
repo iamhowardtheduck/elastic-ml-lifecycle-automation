297cc4e0701035ae6eabafb855d0c63103a998c8
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-05-08 10:22:31 -0400

    Update cluster-config.sh

--- Scripts/cluster-config.sh
+++ Scripts/cluster-config.sh
@@ -26,10 +26,10 @@ curl -X POST "http://kubernetes-vm:30920/_security/user/sdg" -H "Content-Type: a
 
 
 # Install LLM Connector
-bash /opt/workshops/elastic-llm.sh -k false -m gpt-4.1 -d true
+bash /opt/workshops/elastic-llm.sh -m gpt-5.2 -k false -d true -n gpt5-connector -P curriculum-development
 
 echo
-echo "GPT-4.1 AI Connector configured as OpenAI"
+echo "GPT-5.2 AI Connector configured as gpt5-connector"
 echo
 echo 
 echo
